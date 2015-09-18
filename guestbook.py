5243e39ecc501a526ab17183abe26cb97cf5695e
Sarah Smith <sarahgrace89@gmail.com>
2015-09-18 17:01:26 +0200

    Can now add new projects and subprojects

--- guestbook.py
+++ guestbook.py
@@ -6,6 +6,7 @@ from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
+import time
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -20,12 +21,12 @@ DEFAULT_PROJECT_NAME = 'default_project'
 # will be consistent. However, the write rate should be limited to
 # ~1/second.
 
-# def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-#     """Constructs a Datastore key for a Guestbook entity.
+def project_key(project_name=DEFAULT_PROJECT_NAME):
+    """Constructs a Datastore key for a Project entity.
 
-#     We use guestbook_name as the key.
-#     """
-#     return ndb.Key('Guestbook', guestbook_name)
+    We use project_name as the key.
+    """
+    return ndb.Key('Project', project_name)
 
 
 class Author(ndb.Model):
@@ -43,11 +44,12 @@ class Greeting(ndb.Model):
 class Project(ndb.Model):
     """ notes """
     name = ndb.StringProperty(indexed=False)
+    description = ndb.StringProperty(indexed=False)
 
 class SubProject(ndb.Model):
     """ notes """
     name = ndb.StringProperty(indexed=False)
-    parent_project = ndb.StructuredProperty(Project)
+    description = ndb.StringProperty(indexed=False)
 
 class TimeEntry(ndb.Model):
     """ notes """
@@ -91,14 +93,26 @@ class TimeEntry(ndb.Model):
 class ProjectManager(webapp2.RequestHandler):
 
     def get(self):
+        time.sleep(0.1)
         project_query = Project.query()
         projects = project_query.fetch()
+
+        items = list()
+        if projects:
+            for proj in projects:
+                items.append(proj)
+                subproject_query = SubProject.query(ancestor=project_key(proj.name))
+                subprojects = subproject_query.fetch()
+                print subprojects
+                if subprojects:
+                    for subproj in subprojects:
+                        items.append(subproj)
 
         url = users.create_login_url(self.request.uri)
         url_linktext = 'Login'
 
         template_values = {
-            'projects': projects,
+            'items': items,
             'url': url,
             'url_linktext': url_linktext,
         }
@@ -114,18 +128,37 @@ class ProjectManager(webapp2.RequestHandler):
         # ~1/second.
         #project_name = self.request.get('project_name',
         #                                  DEFAULT_PROJECT_NAME)
-        project = Project()
+
+        # guestbook_name = self.request.get('guestbook_name',
+        #                                   DEFAULT_GUESTBOOK_NAME)
+        # greeting = Greeting(parent=guestbook_key(guestbook_name))
 
         #if users.get_current_user():
         #    greeting.author = Author(
         #            identity=users.get_current_user().user_id(),
         #            email=users.get_current_user().email())
+        if not self.request.get('project'):
+            exit("ERROR: No project provided.")
+        else:
+            if self.request.get('subproject'):
+                proj_key = project_key(self.request.get('project'))
+                if proj_key:
+                    subproject = SubProject(parent=proj_key)
+                    subproject.name = self.request.get('subproject')
+                    subproject.description = self.request.get('description')
+                    subproject.put()
+                else:
+                    exit("ERROR: No project with that name exists.")
+            else: #create new project
+                project = Project()
+                project.name = self.request.get('project')
+                project.description = self.request.get('description')
+                project.put()
 
-        project.name = self.request.get('content')
-        project.put()
-
+        # query_params = {'guestbook_name': guestbook_name}
+        # self.redirect('/?' + urllib.urlencode(query_params))
         #query_params = {'guestbook_name': guestbook_name}
-        #self.redirect('/?' + urllib.urlencode(query_params))
+        self.redirect('/?')
 
 
 app = webapp2.WSGIApplication([
