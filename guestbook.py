ff587ddac75ce61564a2a18a1b770dff316efb9f
Sarah Smith <sarahgrace89@gmail.com>
2015-09-25 14:30:22 +0200

    Can select project and subsequent subproject on timer page

--- guestbook.py
+++ guestbook.py
@@ -8,6 +8,7 @@ import jinja2
 import webapp2
 import time
 import logging
+import json
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -124,6 +125,28 @@ class ClockManager(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('clock.html')
         self.response.write(template.render(template_values))
 
+class ProjectLister(webapp2.RequestHandler):
+    def get(self):
+        time.sleep(0.1)
+        project_query = Project.query()
+        projects = project_query.fetch()
+
+        ret_list = []
+        if projects:
+            for proj in projects:
+
+                sub_list = []
+
+                subprojects = SubProject.query(ancestor=proj.key).fetch()
+                if subprojects:
+                    for subproj in subprojects:
+                        sub_list.append({ 'name': subproj.name })
+                ret_list.append({
+                    'name': proj.name,
+                    'subprojects': sub_list
+                })
+        self.response.write(json.dumps(ret_list))
+
 class ProjectManager(webapp2.RequestHandler):
 
     def get(self):
@@ -228,4 +251,5 @@ app = webapp2.WSGIApplication([
     ('/sign', ProjectManager),
     ('/newproject', ProjectManager),
     ('/timeentry', TimeManager),
+    ('/projects', ProjectLister)
 ], debug=True)
